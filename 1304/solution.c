566813448872b824dc2b4035d8bb3af4bdafa9b9
hayadid <yalibrotman@gmail.com>
2025-11-12 22:56:25 +0200

    solved 1304

---
+++ 1304/solution.c
@@ -0,0 +1,28 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+ 
+int* sumZero(int n, int* returnSize) {
+    *returnSize = n;
+
+    int* arr = (int*)malloc(sizeof(int) * n);
+
+    int currentDiff = n / 2;
+
+    int index = 0;
+
+    // If n is odd, loop won't reach middle of array, keeping it zero
+    for (; index < n/2; index++) {
+        arr[index] = currentDiff;
+        arr[n - 1 - index] = -currentDiff;
+        currentDiff--;
+    }
+
+    if (n % 2 == 1) {
+        arr[index] = 0;
+    }
+
+    return arr;
+}
+
+// balls
